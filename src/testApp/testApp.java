07a666b357f95e77039e96b486db381d6a2e53f9
Serhiy Kushniruk <serhiycoo@gmail.com>
2015-11-22 09:46:38 +0200

    test

--- src/testApp/testApp.java
+++ src/testApp/testApp.java
@@ -1,11 +1,34 @@
 package testApp;
 
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.WebDriver;
+import org.junit.Test;
 
 /**
  * Created by Serhiy on 22.11.2015.
  */
 public class testApp {
-    FirefoxDriver driver = new FirefoxDriver();
-    driver.navigate().to("http://google.com");
+    WebDriver driver = new FirefoxDriver();
+
+    @Before
+    public void setup()
+    {
+        driver.navigate().to("http://google.com");
+    }
+
+    @Test
+    public void test()
+    {
+        String str = driver.getTitle();
+        System.out.println(str);
+
+    }
+
+    @After
+    public void teardown()
+    {
+        driver.close();
+    }
 }
