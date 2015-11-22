c9de66d3f5f390f94ad300b0d746ac593bed7281
Serhiy Kushniruk <serhiycoo@gmail.com>
2015-11-22 09:28:21 +0200

    added files

---
+++ src/testApp/testApp.java
@@ -0,0 +1,11 @@
+package testApp;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+/**
+ * Created by Serhiy on 22.11.2015.
+ */
+public class testApp {
+    FirefoxDriver driver = new FirefoxDriver();
+    driver.navigate().to("http://google.com");
+}
